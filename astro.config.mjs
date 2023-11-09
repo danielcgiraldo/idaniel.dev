c70ed66355c554bd71eecd5f9ff85f58c8e772cf
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-11-09 12:55:03 -0500

    astro init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
+
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react(), sitemap()],
+  site: "https://idaniel.dev"
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        fontFamily: {
+            sans: ['"Inter"', "sans-serif"],
+            body: ['"Inter"', "sans-serif"],
+        },
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            white: "#ffffff",
+            midnight: "#000814",
+            "golden-poppy": "#FFC300",
+        },
+    },
+    plugins: [],
+};
