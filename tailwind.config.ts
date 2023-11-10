403884bbb9c9ddc01e47a0bcbb308392e86f3d69
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-11-10 02:16:55 -0500

    Serif

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         fontFamily: {
+            serif: ['"Source Serif 4"', "serif"],
             sans: ['"Inter"', "sans-serif"],
             body: ['"Inter"', "sans-serif"],
         },
